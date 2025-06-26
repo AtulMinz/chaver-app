1734d564a768183c8ab4899984a866fb29f7ba81
Atulya Minz <atulminz.jsr@gmail.com>
2025-06-26 23:52:37 +0530

    Added Pinata

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,22 @@
 import { Hono } from "hono";
 import { GoogleGenAI } from "@google/genai";
 import { env } from "hono/adapter";
+import { PinataSDK } from "pinata";
 
 type Bindings = {
   GEMINI_API: string;
+  PINATA_JWT_TOKEN: string;
+  GATEWAY_URL: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 function main() {
   const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API });
+  const pinata = new PinataSDK({
+    pinataJwt: process.env.PINATA_JWT,
+    pinataGateway: process.env.GATEWAY_URL,
+  });
 
   app.get("/api/health", async (c) => {
     return c.json({
@@ -26,7 +33,11 @@ function main() {
       contents: message as string,
     });
 
-    return c.json({ result: response.text });
+    const url = await pinata.upload.public.createSignedURL({
+      expires: 60,
+    });
+
+    return c.json({ result: response.text, url: url });
   });
 }
 
